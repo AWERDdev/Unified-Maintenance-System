42fc22edc9e8720ce20fef0af6b6c5b045afd24e
AWERDdev <kokorami565@gmail.com>
2026-06-21 21:19:58 +0300

    fixed errors with the cookie paraser

--- server/index.js
+++ server/index.js
@@ -1,33 +1,31 @@
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser'); // 1. Moved to the top
 const app = express();
 const port = process.env.PORT || 3500;
 
+// Middleware
 app.use(cors({
-    
     origin: 'http://localhost:3000',
-
+    credentials: true
 }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser())
+app.use(cookieParser()); // 2. Now this will work perfectly!
 
-const connect_DB = require("./DB/DB")
+// Database
+const connect_DB = require("./DB/DB");
+connect_DB();
 
-connect_DB()
-
+// Routes
 app.get('/', (req, res) => {
   res.json({ message: 'This is the root endpoint Speaking' });
 });
 
 const StaffSignup = require("./AUTH/Staff_AUTH_signUP");
-const cookieParser = require('cookie-parser');
-
-app.use("/auth",StaffSignup)
+app.use("/auth", StaffSignup);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-//http://localhost:3500/
